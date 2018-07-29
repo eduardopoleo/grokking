73eb2c5b52ebdb42c4a7d45b89d82f686ad8a580
Eduardo Poleo <eduardopoleo@gmail.com>
2018-07-29 15:23:27 -0400

    Input validation

--- parse_tree.rb
+++ parse_tree.rb
@@ -27,9 +27,11 @@ class ParseTree
 				current_node.value = token
 				current_node.insert_right('')
 				current_node = current_node.right
-			else
+			elsif token =~ /\A(\d)+\z/ # integers
 				current_node.value = token.to_i
 				current_node = current_node.parent
+			else
+				raise "Not valid character '#{token}'"
 			end
 		end
 
