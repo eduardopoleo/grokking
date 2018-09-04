db844fb3fb3519a13648dcf6e823cf68bb2e1795
Eduardo Poleo <eduardopoleo@gmail.com>
2018-09-04 11:54:25 -0400

    Comments

--- knapsack.rb
+++ knapsack.rb
@@ -1,6 +1,3 @@
-# Total weight is 4lbs
-
-
 KNAPSACK_MAX_WEIGTH = 4
 
 items = [
@@ -10,7 +7,19 @@ items = [
 	{ name: 'stereo', weight: 4, value: 3000 }
 ]
 # The algorithm is based on the formula
+#          1  |  2  |  3  |  4
+# guitar 1500  1500  1500  1500
+# stereo 1500  1500  1500  3000
+# laptop 1500  1500  2000  3500
 
+# cell[i][j] = max of {
+#  The previous max  (value at cell[i-weight][j]])
+#  value current item + value of the remaning space => cell[i-1][weight - item[:weight]]
+# }
+
+# The rest just covers edge cases
+# - if we are checking for the first item
+# - if the current item weights more that the available weight on that cell
 
 def knapsack(items, max_weight)
 	grid = []
@@ -22,6 +31,7 @@ def knapsack(items, max_weight)
 		(0..max_weight).each do |weight|
 			previous_item = i - 1
 
+			# if it's the very first item in the list
 			if previous_item < 0
 				if item[:weight] > weight
 					max_values << { value: 0, items: [] }
@@ -33,24 +43,24 @@ def knapsack(items, max_weight)
 			end
 
 			previous_max = grid[previous_item][weight]
-
 			weight_remaning = weight - item[:weight]
 
+			# the item weights more than the cell allows
 			if weight_remaning < 0
 				max_values << previous_max
 			else
 				max_of_remaining = grid[previous_item][weight_remaning]
 
 				previous_value = previous_max[:value]
-				current_value = max_of_remaining[:value] + item[:value]
+				current_value = item[:value] + max_of_remaining[:value]
 
 				if current_value > previous_value
 					max_values << { 
-						value: max_of_remaining[:value] + item[:value],
-						items: max_of_remaining[:items] + [item[:name]]
+						value: current_value ,
+						items: [item[:name]] + max_of_remaining[:items]
 					}
 				else
-					max_values << previous_max						
+					max_values << previous_max # prioritize the first solution found			
 				end
 			end
 		end
