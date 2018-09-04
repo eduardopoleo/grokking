3e53eb40c38ab1e7d86738fbe43b885b8e2530f7
Eduardo Poleo <eduardopoleo@gmail.com>
2018-09-04 11:20:55 -0400

    recover collection of items

--- knapsack.rb
+++ knapsack.rb
@@ -21,9 +21,9 @@ def knapsack(items, max_weight)
 
 			if previous_item < 0
 				if item[:weight] > weight
-					max_values << 0
+					max_values << { value: 0, items: [] }
 				else
-					max_values << item[:value]
+					max_values << { value: item[:value], items: [item[:name]] }
 				end
 
 				next
@@ -36,8 +36,10 @@ def knapsack(items, max_weight)
 			if weight_remaning < 0
 				max_values << previous_max
 			else
-				v = item[:value] + grid[previous_item][weight_remaning]
-				max_values << v
+				v = item[:value] + grid[previous_item][weight_remaning][:value]
+				objects = grid[previous_item][weight_remaning][:items] << item[:name]
+
+				max_values << { value: v, items: objects}
 			end
 		end
 	end
@@ -45,7 +47,8 @@ def knapsack(items, max_weight)
 	grid
 end
 
-p knapsack(items, KNAPSACK_MAX_WEIGTH)
+require 'pp'
+pp knapsack(items, KNAPSACK_MAX_WEIGTH)
 
 
 
